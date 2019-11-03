1a2934c2fc624c950a61383113469cda81b5d9d2
Georgi Gerganov <ggerganov@gmail.com>
2019-11-03 10:59:38 +0200

    Add cron job comments

--- cron-6-49.sh
+++ cron-6-49.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# toto 2 - 6/49 is played every thursday and sunday at 17:50
+# we add the following cronjobs to fetch the latest results and
+# add them to the archive:
+#
+#  m h dom mon dow user	command
+# 30 18   * * Thu root    /path/to/toto-check/cron-6-49.sh > /dev/null
+# 30 18   * * Sun root    /path/to/toto-check/cron-6-49.sh > /dev/null
+#
+
 cd "$(dirname "$0")"
 
 archive="./data/6-49/archive-all.txt"
@@ -21,5 +30,6 @@ if [ $new_issue != $last_issue ] ; then
     echo $new_data | sed -e "s/.*\.//" >> $archive
 fi
 
+# make sure build folder is available
 cd build
 ./bin/reload-archive-6-49
